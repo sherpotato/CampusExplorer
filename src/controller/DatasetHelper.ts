80297f81eda0a19c8f439a23c72c7910ed5cacd1
Xihan Wu <i4f1b@ugrad.cs.ubc.ca>
2018-10-18 14:55:55 -0700

    start to implement add room dataset

--- src/controller/DatasetHelper.ts
+++ src/controller/DatasetHelper.ts
@@ -34,66 +34,65 @@ export class DatasetHelper {
 
                 Promise.all(promiseList).then((allJSON: any) => {
                     // Log.trace("dealing with all promise...");
-                        for (const eachCourse of allJSON) {
-                            try {
-                                let readableJSON = JSON.parse(eachCourse)["result"];
-                                for (let eachSection of readableJSON) {
-                                    try {
-                                        let year = parseInt(eachSection.Year, 10);
-                                        // let uuid = eachSection.id.toString();
-                                        if (typeof eachSection.Subject === "string" &&
-                                            typeof eachSection.Course === "string" &&
-                                            typeof eachSection.Avg === "number" &&
-                                            typeof eachSection.Professor === "string" &&
-                                            typeof eachSection.Title === "string" &&
-                                            typeof eachSection.Pass === "number" &&
-                                            typeof eachSection.Fail === "number" &&
-                                            typeof eachSection.Audit === "number" &&
-                                            typeof eachSection.id.toString() === "string" &&
-                                            typeof year === "number") {
-                                            // Log.trace("Section Keys' Type checked");
-                                            const validSection: { [key: string]: string | number } = {
-                                                // TO DO: ASK TA!!!
-                                                [id + "_dept"]: eachSection.Subject,
-                                                [id + "_id"]: eachSection.Course,
-                                                [id + "_avg"]: eachSection.Avg,
-                                                [id + "_instructor"]: eachSection.Professor,
-                                                [id + "_title"]: eachSection.Title,
-                                                [id + "_pass"]: eachSection.Pass,
-                                                [id + "_fail"]: eachSection.Fail,
-                                                [id + "_audit"]: eachSection.Audit,
-                                                [id + "_uuid"]: eachSection.id.toString(),
-                                                [id + "_year"]: year,
-                                            };
-                                            validJSON.push(validSection);
-                                        }
-                                    } catch {
-                                        // error parsing a section
+                    for (const eachCourse of allJSON) {
+                        try {
+                            let readableJSON = JSON.parse(eachCourse)["result"];
+                            for (let eachSection of readableJSON) {
+                                try {
+                                    let year = parseInt(eachSection.Year, 10);
+                                    // let uuid = eachSection.id.toString();
+                                    if (typeof eachSection.Subject === "string" &&
+                                        typeof eachSection.Course === "string" &&
+                                        typeof eachSection.Avg === "number" &&
+                                        typeof eachSection.Professor === "string" &&
+                                        typeof eachSection.Title === "string" &&
+                                        typeof eachSection.Pass === "number" &&
+                                        typeof eachSection.Fail === "number" &&
+                                        typeof eachSection.Audit === "number" &&
+                                        typeof eachSection.id.toString() === "string" &&
+                                        typeof year === "number") {
+                                        // Log.trace("Section Keys' Type checked");
+                                        const validSection: { [key: string]: string | number } = {
+                                            // TO DO: ASK TA!!!
+                                            [id + "_dept"]: eachSection.Subject,
+                                            [id + "_id"]: eachSection.Course,
+                                            [id + "_avg"]: eachSection.Avg,
+                                            [id + "_instructor"]: eachSection.Professor,
+                                            [id + "_title"]: eachSection.Title,
+                                            [id + "_pass"]: eachSection.Pass,
+                                            [id + "_fail"]: eachSection.Fail,
+                                            [id + "_audit"]: eachSection.Audit,
+                                            [id + "_uuid"]: eachSection.id.toString(),
+                                            [id + "_year"]: year,
+                                        };
+                                        validJSON.push(validSection);
                                     }
+                                } catch {
+                                    // error parsing a section
                                 }
+                            }
 
-                            } catch {
-                                // Something wrong with a JSON
-                                // TO DO: ask TA!!!!!!!
-                            }
+                        } catch {
+                            // Something wrong with a JSON
                         }
-                        if (validJSON.length === 0) {
-                            let e = new InsightError("Dataset is not valid.");
-                            return reject(e);
-                        } else {
-                            const fs = require("fs");   // import file system
-                            try {
-                                fs.mkdir("./data", () => {
-                                    const courseString = JSON.stringify(validJSON, null, " ");
-                                    fs.writeFile("./data/" + id + ".json", courseString);
-                                    datasetId.push(id);
-                                    validDataset.set(id, validJSON);
-                                    fulfill(datasetId);
-                                });
-                            } catch {
-                                reject(new InsightError("Fail to add this dataset into cache!"));
-                            }
+                    }
+                    if (validJSON.length === 0) {
+                        let e = new InsightError("Dataset is not valid.");
+                        return reject(e);
+                    } else {
+                        const fs = require("fs");   // import file system
+                        try {
+                            fs.mkdir("./data", () => {
+                                const courseString = JSON.stringify(validJSON, null, " ");
+                                fs.writeFile("./data/" + id + ".json", courseString);
+                                datasetId.push(id);
+                                validDataset.set(id, validJSON);
+                                fulfill(datasetId);
+                            });
+                        } catch {
+                            reject(new InsightError("Fail to add this dataset into cache!"));
                         }
+                    }
 
                 });
             }).catch((e) => {
@@ -108,10 +107,19 @@ export class DatasetHelper {
                           datasetId: string[], validDataset: Map<string, any[]>): Promise<string[]> {
         return new Promise((fulfill, reject) => {
             let currZip = new JSZip();
-            const promiseList: Array<Promise<string>> = [];
-// load dataset
+            const parse5 = require("parse5");
+            // load dataset
             currZip.loadAsync(content, {base64: true}).then((unzippedFiles: any) => {
-                return reject (); // TODO
+                unzippedFiles.file("index.htm").async("text").then((index: any) => {
+                    try {
+                        const inputIndex = parse5.parse(index);
+                        // TODO
+                    } catch (e) {
+                        reject(new InsightError(""));
+                    }
+                }).catch(() => {
+                    reject(new InsightError("index.htm does not exist."));
+                });
             }).catch((e) => {
                 e = new InsightError("Fail to unzip the file.");
                 reject(e);
