22226434d9301e4dc8e8f4f83a84a175408391d3
Xihan Wu <i4f1b@ugrad.cs.ubc.ca>
2018-11-07 21:01:58 -0800

    build query first

--- frontend/public/query-builder.js
+++ frontend/public/query-builder.js
@@ -8,6 +8,36 @@
 CampusExplorer.buildQuery = function() {
     let query = {};
     // TODO: implement!
+    let content = document.getElementsByClassName("tab-panel active")[0];
+
+    // Find logical type
+    let logicalType = findLogicalType(content.getElementsByClassName("control-group condition-type")[0]);
+
+    // Find Logical condition(s)
+    let condition = content.getElementsByClassName("control-group condition");
+    // if (condition.length === 0){
+    // empty where
+    // } else if (condition.length)
+
+
+
     console.log("CampusExplorer.buildQuery not implemented yet.");
     return query;
 };
+
+// helper for finding logical type
+findLogicalType = function(condition){
+    // ALL
+    if (condition.getElementsByClassName("control conditions-all-radio")[0].getElementsByTagName("input")[0].checked){
+        return "AND";
+    }
+    // ANY
+    if (condition.getElementsByClassName("control conditions-any-radio")[0].getElementsByTagName("input")[0].checked){
+        return "OR";
+    }
+    // NOT
+    else{
+        return "NOT";
+    }
+};
+
